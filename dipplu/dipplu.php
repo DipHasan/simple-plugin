d451f48ea1ed743c1ecf7faf6886d80e9602860e
DipHasan <44706237+DipHasan@users.noreply.github.com>
2020-10-23 19:15:03 +0600

    upload

---
+++ dipplu/dipplu.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * Plugin Name: Dip Plu
+ * Description: It is A simple description of our plguin
+ * Plugin URI: http://diphasan.me/
+ * Author: Dip Hasan
+ * Author URI: http://diphasan.me/
+ * Version: 0.1
+ * License: GPL2
+ * Text Domain: dipplu
+ */
+
+/**
+ * Copyright (c) 2020 Dip Hasan (email: mddip01@gmail.com). All rights reserved.
+ *
+ * Released under the GPL license
+ * http://www.opensource.org/licenses/gpl-license.php
+ *
+ * This is an add-on for WordPress
+ * http://wordpress.org/
+ *
+ * **********************************************************************
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ * **********************************************************************
+ */
+
+// don't call the file directly
+if(!defined('ABSPATH')) exit;
+
+echo "hello world";
